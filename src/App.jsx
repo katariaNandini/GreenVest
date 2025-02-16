5bda4abcdd7a1c15c7272fc9c1dfe8f719cbb022
katariaNandini <nk366@snu.edu.in>
2025-02-16 07:28:23 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@
 // }
 
 // export default App
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import LandingPage from './LandingPage'
@@ -26,9 +26,29 @@ import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import ProtectedRoute from './components/auth/ProtectedRoute'
 import GreenFundSearch from './components/GreenFundSearch'
-import BuyStock from './components/BuyStock'
-import TransactionPage from './components/TransactionPage'
 
+function App() {
+  return (
+    <Router>
+      <div className='overflow-x-hidden'>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route 
+            path="/features" 
+            element={
+              <ProtectedRoute>
+                <FeaturePage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route path="/green-funds" element={<GreenFundSearch />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
 // function App() {
 //   return (
 //     <Router>
@@ -37,35 +57,12 @@ import TransactionPage from './components/TransactionPage'
 //           <Route path="/" element={<LandingPage />} />
 //           <Route path="/login" element={<Login />} />
 //           <Route path="/signup" element={<Signup />} />
-//           <Route 
-//             path="/features" 
-//             element={
-//               <ProtectedRoute>
-//                 <FeaturePage />
-//               </ProtectedRoute>
-//             } 
-//           />
+//           <Route path="/features" element={<FeaturePage />} />
 //           <Route path="/green-funds" element={<GreenFundSearch />} />
 //         </Routes>
 //       </div>
-//     </Router>
-//   )
+//     </Router>
+//   )
 // }
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/features" element={<FeaturePage />} />
-        <Route path="/green-funds" element={<GreenFundSearch />} />
-        <Route path="/invest" element={<BuyStock />} />
-        <Route path="/funds" element={<GreenFundSearch />} />
-        <Route path="/transaction" element={<TransactionPage />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
 
 export default App
