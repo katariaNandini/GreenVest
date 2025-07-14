adfd51e43fbe66fdcb4aa83d63e225641c5d5975
katariaNandini <nk366@snu.edu.in>
2025-07-14 13:40:48 +0530

    auth fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// import LandingPage from './LandingPage'
-// import Footer from './Footer'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//      <div className='overflow-x-hidden'>
-//     <LandingPage /> 
-//     </div>
-//   )
-// }
-
-// export default App
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import LandingPage from './LandingPage'
 import Footer from './Footer'
 import FeaturePage from './FeaturePage'
-import Login from './components/auth/Login'
-import Signup from './components/auth/Signup'
-import ProtectedRoute from './components/auth/ProtectedRoute'
+// import Login from './components/auth/Login'
+// import Signup from './components/auth/Signup'
+// import ProtectedRoute from './components/auth/ProtectedRoute'
 import GreenFundSearch from './components/GreenFundSearch'
 
 function App() {
@@ -33,37 +13,14 @@ function App() {
       <div className='overflow-x-hidden'>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route 
-            path="/features" 
-            element={
-              <ProtectedRoute>
-                <FeaturePage />
-              </ProtectedRoute>
-            } 
-          />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/signup" element={<Signup />} /> */}
+          <Route path="/features" element={<FeaturePage />} />
           <Route path="/green-funds" element={<GreenFundSearch />} />
         </Routes>
       </div>
     </Router>
   )
 }
-// try commit
-// function App() {
-//   return (
-//     <Router>
-//       <div className='overflow-x-hidden'>
-//         <Routes>
-//           <Route path="/" element={<LandingPage />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/signup" element={<Signup />} />
-//           <Route path="/features" element={<FeaturePage />} />
-//           <Route path="/green-funds" element={<GreenFundSearch />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   )
-// }
 
 export default App
